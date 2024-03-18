cc250727521058ff3f6190517e0461384f99558a
Tyler Magoteaux <123842019+SullySquirrel@users.noreply.github.com>
2024-03-18 01:43:07 -0400

    New files added

---
+++ CCA111 Project/javascript/projJs.js
@@ -0,0 +1,6 @@
+let slideIndex = 1;
+
+function hideImage() {
+    document.getElementById("star-button").style.visibility = "hidden";
+    document.getElementById("link").style.visibility = "visible";  
+}
