3a22922f43b3ca77c78c7ff82cbcf42dd5d85cc0
Tyler Magoteaux <123842019+SullySquirrel@users.noreply.github.com>
2024-03-18 01:34:01 -0400

    Delete javascript directory

--- javascript/projJs.js
+++
@@ -1,6 +0,0 @@
-let slideIndex = 1;
-
-function hideImage() {
-    document.getElementById("star-button").style.visibility = "hidden";
-    document.getElementById("link").style.visibility = "visible";  
-}
